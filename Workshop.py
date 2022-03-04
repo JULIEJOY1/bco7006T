869aadaf27bad585c9c8153f6362d9c4f01fbc81
ASUS <julie.joy@live.vu.edu.au>
2022-03-04 20:10:52 +0530

    first file3

--- Workshop.py
+++ Workshop.py
@@ -1,3 +1,6 @@
-#put your name in the variable below
-name = "julie joy"
-print("Hello", name)
+width = float(input('Please Enter the Width of a Rectangle: '))
+height = float(input('Please Enter the Height of a Rectangle: '))
+
+Area = width * height
+
+print("\n Area of a Rectangle is:%.2f" %Area)
